7cef5419a90f486609d47a51b51db9553a062c9e
guimaraessilas <guimaraes.silas2013@gmail.com>
2018-06-12 20:57:49 -0300

    packages updated

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,6 +1,7 @@
 // Libs
 import 'angular-animate';
 import 'angular-meteor';
+import 'angular-meteor-auth';
 import 'angular-moment';
 import 'angular-sanitize';
 import 'angular-ui-router';
@@ -12,7 +13,9 @@ import { Meteor } from 'meteor/meteor';
 // Modules
 import RoutesConfig from '../routes';
 import CalendarFilter from '../filters/calendar.filter';
+import NewChatService from '../services/new-chat.service';
 import InputDirective from '../directives/input.directive';
+import NewChatCtrl from '../controllers/new-chat.controller';
 import ChatsCtrl from '../controllers/chats.controller';
 import ChatCtrl from '../controllers/chat.controller';
 
@@ -22,6 +25,7 @@ const App = 'Whatsapp';
 // App
 Angular.module(App, [
   'angular-meteor',
+  'angular-meteor.auth',
   'angularMoment',
   'ionic'
 ]);
@@ -29,8 +33,10 @@ Angular.module(App, [
 new Loader(App)
     .load(ChatsCtrl)
     .load(ChatCtrl)
+    .load(NewChatCtrl)
     .load(InputDirective)
     .load(CalendarFilter)
+    .load(NewChatService)
     .load(RoutesConfig);
 
 // Startup
